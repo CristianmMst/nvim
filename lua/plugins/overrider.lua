c09b369c275f30be0ad2ae1437d88b9276a35e28
CristianmMst <cristianmmst@gmail.com>
2023-12-09 15:33:42 -0500

    add formmating php

--- lua/plugins/overrider.lua
+++ lua/plugins/overrider.lua
@@ -1,5 +1,29 @@
 return {
   {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      table.insert(opts.ensure_installed, "pint")
+    end,
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      opts.sources = opts.sources or {}
+      table.insert(opts.sources, nls.builtins.formatting.pint)
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        ["php"] = { "pint" },
+      },
+    },
+  },
+  {
     "LazyVim/Lazyvim",
     opts = {
       colorscheme = "catppuccin",
