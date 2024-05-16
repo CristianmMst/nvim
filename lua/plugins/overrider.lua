ce807a71fd9417eb3ca88334c42304f2148b37ce
CristianmMst <cristianmmst@gmail.com>
2024-05-16 00:15:23 -0500

    remove autopep8

--- lua/plugins/overrider.lua
+++ lua/plugins/overrider.lua
@@ -21,7 +21,6 @@ return {
       local nls = require("null-ls")
       opts.sources = opts.sources or {}
       table.insert(opts.sources, nls.builtins.formatting.pint)
-      table.insert(opts.sources, nls.builtins.formatting.autopep8)
     end,
   },
   {
