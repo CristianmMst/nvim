3e5ece177b6dbfa7f527d71e9134aa11118b713e
CristianmMst <cristianmmst@gmail.com>
2024-11-22 19:20:41 -0500

    add dashboard config

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -24,6 +24,7 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
+    { import = "plugins.ui" },
     { import = "plugins.coding" },
   },
   defaults = {
